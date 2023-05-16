95ff3b505c14060a938aca863e2213d5ed1cf67e
AlexLaptop <alex03marcos@gmail.com>
2023-05-16 08:33:11 +0200

    primera version

---
+++ src/cliente/ClienteTrivial.java
@@ -0,0 +1,49 @@
+package cliente;
+
+import servidor.EnviarMensaje;
+import servidor.RecibirMensaje;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ClienteTrivial {
+    public static void main(String[] args) throws Exception {
+        // crear socket de cliente para conectarse al servidor
+        Socket socket = new Socket("localhost", 12345);
+
+        // recibir la pregunta "Cómo te llamas?" y enviar la respuesta
+        String pregunta = RecibirMensaje.recibir(socket);
+        System.out.println(pregunta);
+        String respuesta = leerRespuestaDesdeConsola();
+        EnviarMensaje.enviar(socket, respuesta);
+
+        // recibir las primeras 5 preguntas y enviar las respuestas correspondientes
+        int puntuacionTotal = 0;
+        String bienvenida = RecibirMensaje.recibir(socket);
+        System.out.println(bienvenida);
+        for (int i = 0; i < 5; i++) {
+            String preguntaActual = RecibirMensaje.recibir(socket); // recibir pregunta
+            System.out.println(preguntaActual); // mostrar pregunta
+            String respuestaActual = leerRespuestaDesdeConsola(); // leer respuesta desde la consola
+            EnviarMensaje.enviar(socket, respuestaActual); // enviar respuesta al servidor
+            int puntuacion = Integer.parseInt(RecibirMensaje.recibir(socket)); // recibir la puntuación obtenida
+            puntuacionTotal += puntuacion; // actualizar la puntuación total
+            System.out.println("Respuesta: " + (puntuacion == 1 ? "Correcto!" : "Incorrecto.") +
+                    " Puntuación: " + puntuacion + "\n"); // mostrar resultado
+        }
+
+        // recibir el mensaje de despedida con la puntuación total
+        String mensajeDespedida = RecibirMensaje.recibir(socket);
+        System.out.println(mensajeDespedida + " Tu puntuación final es: " + puntuacionTotal);
+
+        // cerrar el socket
+        socket.close();
+    }
+
+    private static String leerRespuestaDesdeConsola() throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Respuesta: ");
+        return in.readLine();
+    }
+}
